baf74be7d966df3dcd65e1d74b4e950da9e49357
Hongfei Li <fever324@gmail.com>
2015-10-25 00:11:27 -0400

    Use nltk to preprocess instead of en from nodebox

--- Project2/utils.py
+++ Project2/utils.py
@@ -1,25 +1,28 @@
 from nltk import pos_tag, word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
+import nltk
 import en
 import sys
 
 
 def process_string(string, unwantedTags):
     # Remove irrelevent words such as 'and', 'the'
-    string = remove_unwanted_tags(string, unwantedTags)
+    tags = remove_unwanted_tags(string.lower(), unwantedTags)
 
     # Turn sentence to single and present tense for easier analysis
-    string = sentence_to_present_tense_and_single(string)
-    return string
+    tags = sentence_to_present_tense_and_single(tags)
+    return turn_tags_to_string(tags)
 
 
 def remove_unwanted_tags(string, unwantedTags):
 
     wordTagPairs = pos_tag(word_tokenize(string))
-    returnString = ''
+    returnTags = []
     for word in wordTagPairs:
-        if(word[1] not in unwantedTags):
-            returnString += word[0] + ' '
-    return returnString
+        if(word[1] not in unwantedTags) and len(word[0]) != 1 and word[0] != 'n\'t' and word[0] != '\'s':
+            returnTags += word
+    return returnTags
 
 
 def construct_unwanted_tags():
@@ -33,22 +36,63 @@ def construct_unwanted_tags():
     return unwantedTags
 
 
-def sentence_to_present_tense_and_single(string):
-    words = string.split()
-    returnString = ''
-    for word in words:
-        # Remove single letter word
-        if word == 'n\'t':
-            continue
-        if len(word) != 1:
-            try:
-                if en.is_noun(word):
-                    returnString += ' ' + en.noun.singular(word)
-                elif en.is_verb(word):
-                    returnString += ' ' + en.verb.present(word)
-                else:
-                    returnString += ' ' + word
-            except:
-                print 'cannot process word ' + word
+def sentence_to_present_tense_and_single(tags):
+    for i in range(len(tags)):
+        wn_tag = penn_to_wn(tags[i][1])
+        tags[i] = WordNetLemmatizer().lemmatize(tags[i][0], wn_tag)
+    return tags
 
-    return returnString.strip()
+
+def turn_tags_to_string(tags):
+    toReturn = ''
+    for tag in tags:
+        toReturn += tag[0]
+    return toReturn
+
+# def sentence_to_present_tense_and_single(string):
+#     words = string.split()
+#     returnString = ''
+#     for word in words:
+#         # Remove single letter word
+#         if word == 'n\'t':
+#             continue
+#         if len(word) != 1:
+#             try:
+#                 if en.is_noun(word):
+#                     returnString += ' ' + en.noun.singular(word)
+#                 elif en.is_verb(word):
+#                     returnString += ' ' + en.verb.present(word)
+#                 else:
+#                     returnString += ' ' + word
+#             except:
+#                 print 'cannot process word ' + word
+
+#     return returnString.strip()
+
+
+def is_noun(tag):
+    return tag in ['NN', 'NNS', 'NNP', 'NNPS']
+
+
+def is_verb(tag):
+    return tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+
+def is_adverb(tag):
+    return tag in ['RB', 'RBR', 'RBS']
+
+
+def is_adjective(tag):
+    return tag in ['JJ', 'JJR', 'JJS']
+
+
+def penn_to_wn(tag):
+    if is_adjective(tag):
+        return wn.ADJ
+    elif is_noun(tag):
+        return wn.NOUN
+    elif is_adverb(tag):
+        return wn.ADV
+    elif is_verb(tag):
+        return wn.VERB
+    return wn.NOUN
