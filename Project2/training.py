69b189dec154421fa13d20fb9c54e66d5783ec3b
AaronZang <sz428@cornell.edu>
2015-10-25 00:16:03 -0400

    training.py

--- Project2/training.py
+++ Project2/training.py
@@ -3,6 +3,12 @@ from generateWordMap import get_word_definition_overlap_count
 #  import pdb
 #  pdb.set_trace()
 
+# Feature range around target word
+select_range = 6
+
+# The number of features for a lexelt
+feature_num = 20
+
 
 def naive_bayes_training(root):
     #  Compute P(senseid_i)
@@ -14,7 +20,7 @@ def naive_bayes_training(root):
                 if answer.tag == 'context':
                     continue
 
-                item = lexelt.attrib['item']
+                item = lexelt.attrib['item'].strip().split('.')[0]
                 if item in prior_prob:
                     senseid = answer.attrib['senseid']
                     if senseid == 'U':
@@ -29,13 +35,10 @@ def naive_bayes_training(root):
                 else:
                     prior_prob[item] = {answer.attrib['senseid']: 1}
 
-    senseid_amount = {}
-
     for lexelt in prior_prob:
         count = 0
         for senseid in prior_prob[lexelt]:
             count += prior_prob[lexelt][senseid]
-        senseid_amount[senseid] = count
         for senseid in prior_prob[lexelt]:
             prior_prob[lexelt][senseid] /= float(count)
 
@@ -44,21 +47,21 @@ def naive_bayes_training(root):
     #  Compute P(feature_i|senseid_i)
     feature_candidates_dict = {}
     for lexelt in root:
-        lexelt_item = lexelt.attrib['item'].strip().split()[0]
+        lexelt_item = lexelt.attrib['item'].strip().split('.')[0]
         if lexelt_item not in feature_candidates_dict:
             feature_candidates_dict[lexelt_item] = {}
 
         for instance in lexelt:
             length = len(instance) - 1
             context = instance[length].text.strip().split()
-            feature_candidates = context[-10:]
+            feature_candidates = context[-(select_range/2):]
 
-            context_num = 0
-            while len(feature_candidates) <= 20 and context_num < len(instance[length]):
-                feature_candidates += instance[length][context_num].tail
-                context_num += 1
+            for head in instance[length]:
+                feature_candidates += head.tail.strip().split()
+                if len(feature_candidates) > select_range:
+                    break
 
-            feature_candidates = feature_candidates[:20]
+            feature_candidates = feature_candidates[:select_range]
 
             for answer in instance[:length]:
                 senseid = answer.attrib['senseid']
@@ -77,8 +80,7 @@ def naive_bayes_training(root):
         print lexelt_item + ": Feature candidates done"
 
     print "Feature candidates all done"
-    print feature_candidates_dict
-'''
+
     feature_dict = {}
     for lexelt_item in feature_candidates_dict:
         if lexelt_item not in feature_dict:
@@ -86,22 +88,26 @@ def naive_bayes_training(root):
 
         for feature in feature_candidates_dict[lexelt_item]:
             feature_dict[lexelt_item][feature] = get_word_definition_overlap_count(feature, lexelt_item)
+            print feature + ' overlap ' + lexelt_item + ': ' + str(feature_dict[lexelt_item][feature])
 
-        sorted_features = sorted(feature_dict[lexelt_item], key=feature_dict[lexelt_item].get)[:20]
+        sorted_features = sorted(feature_dict[lexelt_item], key=feature_dict[lexelt_item].get, reverse=True)[:feature_num]
+
+        print lexelt_item + ' selected features: ' + str(sorted_features)
+
         feature_dict[lexelt_item] = {}
 
         for feature in sorted_features:
-            senseid_count_list = feature_candidates_dict[lexelt][feature]
+            senseid_count_list = feature_candidates_dict[lexelt_item][feature]
 
             for senseid_count in senseid_count_list:
                 if not senseid_count[0] in feature_dict[lexelt_item]:
                     feature_dict[lexelt_item][senseid_count[0]] = {}
-                feature_dict[lexelt_item][senseid_count[0]][feature] = senseid_count[1] / senseid_amount[senseid_count[0]]
+                feature_dict[lexelt_item][senseid_count[0]][feature] = senseid_count[1] / prior_prob[lexelt_item][senseid_count[0]]
 
         print lexelt_item + ": prob calc done"
 
     return feature_dict
-'''
+
 
 if __name__ == "__main__":
     tree = ET.parse('processed_training.xml')
